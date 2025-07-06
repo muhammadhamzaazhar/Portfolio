6251815c229c507a7fac001117b3bf3b91b410cc
muhammadhamzaazhar <hamzaazharmuhammad@gmail.com>
2025-07-06 18:52:54 +0500

    modifying data.jsx, assets and portfolio section

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -1,12 +1,86 @@
-import React from "react";
+// import React from "react";
+// import { HiArrowUpRight } from "react-icons/hi2";
+// import { motion } from "framer-motion";
+// import { FiGithub, FiEye } from "react-icons/fi";
+
+// import { workImagesDev } from "../../utils/data";
+// import { workImagesAI } from "../../utils/data";
+// import "./portfolio.styles.scss";
+
+// const Portfolio = () => {
+//   return (
+//     <div className="container" id="portfolio">
+//       <motion.div
+//         initial={{ opacity: 0 }}
+//         whileInView={{ y: [-50, 0], opacity: 1 }}
+//         className="title"
+//       >
+//         <span>My Work</span>
+//         <h1>Awesome Projects</h1>
+//       </motion.div>
+//       <motion.div
+//         initial={{ x: 0, opacity: 0 }}
+//         whileInView={{ x: [-250, 0], opacity: 1 }}
+//         transition={{ duration: 1 }}
+//         exit={{ opacity: 0, y: -50 }}
+//         className="workImages"
+//       >
+//         {workImagesDev.map((work) => {
+//           return (
+//             <div className="workImage" key={work.id}>
+//               <img src={work.img} alt="workImg" />
+//               <motion.div
+//                 initial={{ opacity: 0 }}
+//                 whileHover={{ opacity: [0, 1] }}
+//                 transition={{ duration: 0.3, ease: "easeInOut" }}
+//                 className="hoverLayer"
+//               >
+//                 <p>
+//                   {work.projectName} <HiArrowUpRight size={20} />
+//                 </p>
+//                 <motion.a
+//                   href={work.sourceCode}
+//                   target="_blank"
+//                   whileInView={{ scale: [0, 1] }}
+//                   whileHover={{ scale: [1, 1.1] }}
+//                   transition={{ duration: 0.3 }}
+//                 >
+//                   <FiGithub />
+//                 </motion.a>
+
+//                 <motion.a
+//                   href={work.deploy}
+//                   target="_blank"
+//                   whileInView={{ scale: [0, 1] }}
+//                   whileHover={{ scale: [1, 1.1] }}
+//                   transition={{ duration: 0.3 }}
+//                 >
+//                   <FiEye />
+//                 </motion.a>
+//               </motion.div>
+//             </div>
+//           );
+//         })}
+//       </motion.div>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
+
+import React, { useState } from "react";
 import { HiArrowUpRight } from "react-icons/hi2";
 import { motion } from "framer-motion";
 import { FiGithub, FiEye } from "react-icons/fi";
 
-import { workImages } from "../../utils/data";
+import { workImagesDev, workImagesAI } from "../../utils/data";
 import "./portfolio.styles.scss";
 
 const Portfolio = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Dev");
+  const currentWorkImages =
+    selectedCategory === "Dev" ? workImagesDev : workImagesAI;
+
   return (
     <div className="container" id="portfolio">
       <motion.div
@@ -17,6 +91,27 @@ const Portfolio = () => {
         <span>My Work</span>
         <h1>Awesome Projects</h1>
       </motion.div>
+
+      <div className="buttons">
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          className={selectedCategory === "Dev" ? "active" : ""}
+          onClick={() => setSelectedCategory("Dev")}
+        >
+          Web Development
+        </motion.button>
+
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          className={selectedCategory === "AI" ? "active" : ""}
+          onClick={() => setSelectedCategory("AI")}
+        >
+          Artifical Intelligence
+        </motion.button>
+      </div>
+
       <motion.div
         initial={{ x: 0, opacity: 0 }}
         whileInView={{ x: [-250, 0], opacity: 1 }}
@@ -24,42 +119,39 @@ const Portfolio = () => {
         exit={{ opacity: 0, y: -50 }}
         className="workImages"
       >
-        {workImages.map((work) => {
-          return (
-            <div className="workImage" key={work.id}>
-              <img src={work.img} alt="workImg" />
-              <motion.div
-                initial={{ opacity: 0 }}
-                whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.3, ease: "easeInOut" }}
-                className="hoverLayer"
+        {currentWorkImages.map((work) => (
+          <div className="workImage" key={work.id}>
+            <img src={work.img} alt="workImg" />
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileHover={{ opacity: [0, 1] }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="hoverLayer"
+            >
+              <p>
+                {work.projectName} <HiArrowUpRight size={20} />
+              </p>
+              <motion.a
+                href={work.sourceCode}
+                target="_blank"
+                whileInView={{ scale: [0, 1] }}
+                whileHover={{ scale: [1, 1.1] }}
+                transition={{ duration: 0.3 }}
               >
-                <p>
-                  {work.projectName} <HiArrowUpRight size={20} />
-                </p>
-                <motion.a
-                  href={work.sourceCode}
-                  target="_blank"
-                  whileInView={{ scale: [0, 1] }}
-                  whileHover={{ scale: [1, 1.1] }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <FiGithub />
-                </motion.a>
-
-                <motion.a
-                  href={work.deploy}
-                  target="_blank"
-                  whileInView={{ scale: [0, 1] }}
-                  whileHover={{ scale: [1, 1.1] }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <FiEye />
-                </motion.a>
-              </motion.div>
-            </div>
-          );
-        })}
+                <FiGithub />
+              </motion.a>
+              <motion.a
+                href={work.deploy}
+                target="_blank"
+                whileInView={{ scale: [0, 1] }}
+                whileHover={{ scale: [1, 1.1] }}
+                transition={{ duration: 0.3 }}
+              >
+                <FiEye />
+              </motion.a>
+            </motion.div>
+          </div>
+        ))}
       </motion.div>
     </div>
   );
--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -24,13 +24,17 @@ import {
 import { AiFillGithub } from "react-icons/ai";
 import { SiPostman, SiExpress, SiTailwindcss } from "react-icons/si";
 
-import work1 from "../assets/crwn-clothing.png";
-import work2 from "../assets/chat-app.png";
-import work3 from "../assets/blog-website.png";
-import work4 from "../assets/monsters-rolodex.png";
-import work5 from "../assets/netflix-clone.png";
-import work6 from "../assets/admin-dashboard.png";
-import work7 from "../assets/music-website.png";
+import work1Dev from "../assets/crwn-clothing.png";
+import work2Dev from "../assets/chat-app.png";
+import work3Dev from "../assets/blog-website.png";
+import work4Dev from "../assets/monsters-rolodex.png";
+import work5Dev from "../assets/netflix-clone.png";
+import work6Dev from "../assets/admin-dashboard.png";
+import work7Dev from "../assets/music-website.png";
+import work1AI from "../assets/langbot.png";
+import work2AI from "../assets/deepfake-video-detection.png";
+import work3AI from "../assets/social-profile-analyzer.png";
+import work4AI from "../assets/food-classification.png";
 
 export const navLinks = ["home", "about", "skills", "portfolio", "contact"];
 
@@ -47,7 +51,7 @@ export const socialIcons = [
   },
   {
     icon: <FaLinkedin />,
-    link: "https://www.linkedin.com/in/hamza-azhar-0b7815242/",
+    link: "https://www.linkedin.com/in/muhammad-hamza-azhar-996289314/",
     name: "LinkedIn",
   },
   {
@@ -100,10 +104,10 @@ export const icons = [
   { icon1: <SiPostman />, name: "Postman" },
 ];
 
-export const workImages = [
+export const workImagesDev = [
   {
     id: 1,
-    img: work1,
+    img: work1Dev,
     name: "project 1",
     projectName: "E-commerce App",
     deploy: "https://react-crwn-clothing-app.netlify.app/",
@@ -111,7 +115,7 @@ export const workImages = [
   },
   {
     id: 2,
-    img: work2,
+    img: work2Dev,
     name: "project 2",
     projectName: "Chat App",
     deploy: "https://node-chat-room-8079e4634229.herokuapp.com/",
@@ -119,14 +123,14 @@ export const workImages = [
   },
   {
     id: 3,
-    img: work3,
+    img: work3Dev,
     name: "project 3",
     projectName: "Blog Website",
     sourceCode: "https://github.com/muhammadhamzaazhar/blog-website",
   },
   {
     id: 4,
-    img: work4,
+    img: work4Dev,
     name: "project 4",
     projectName: "Monsters Rolodex",
     deploy: "https://muhammadhamzaazhar.github.io/monsters-rolodex/",
@@ -134,7 +138,7 @@ export const workImages = [
   },
   {
     id: 5,
-    img: work5,
+    img: work5Dev,
     name: "project 5",
     projectName: "Netflix Clone",
     deploy: "https://react-netflix-2023.netlify.app/",
@@ -142,7 +146,7 @@ export const workImages = [
   },
   {
     id: 6,
-    img: work6,
+    img: work6Dev,
     name: "project 6",
     projectName: "Admin Dashboard",
     deploy: "https://admin-dashboard-2024.netlify.app/",
@@ -150,11 +154,50 @@ export const workImages = [
   },
   {
     id: 7,
-    img: work7,
+    img: work7Dev,
     name: "project 7",
     projectName: "Music Website",
     deploy: "https://beatboxify.netlify.app/",
     sourceCode: "https://github.com/muhammadhamzaazhar/Music-Website",
+  },
+];
+
+export const workImagesAI = [
+  {
+    id: 1,
+    img: work1AI,
+    name: "project 1",
+    projectName: "LangChat Docs Helper",
+    deploy: "https://langchain-docs-guide.streamlit.app/",
+    sourceCode:
+      "https://github.com/muhammadhamzaazhar/LangBot-Your-LangChain-Guide",
+  },
+  {
+    id: 2,
+    img: work2AI,
+    name: "project 2",
+    projectName: "Deepfake Video Detection",
+    deploy: "https://skintegrity-deepfake-detection.vercel.app/",
+    sourceCode:
+      "https://github.com/muhammadhamzaazhar/DeepFake-Video-Detection",
+  },
+  {
+    id: 3,
+    img: work3AI,
+    name: "project 3",
+    projectName: "Social Profile Analyzer",
+    deploy: "https://huggingface.co/spaces/mhamza-007/Social-Profile-Analyzer",
+    sourceCode:
+      "https://huggingface.co/spaces/mhamza-007/Social-Profile-Analyzer/tree/main",
+  },
+  {
+    id: 4,
+    img: work4AI,
+    name: "project 4",
+    projectName: "Food Classification",
+    deploy: "https://food-classification-101.netlify.app/",
+    sourceCode:
+      "https://github.com/muhammadhamzaazhar/Food-Classification-using-Deep-Learning",
   },
 ];
 
